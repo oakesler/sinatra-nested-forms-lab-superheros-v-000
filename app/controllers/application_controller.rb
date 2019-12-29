f0d3f7060227e03c7636b8ac6b4fffd0b80ccd3c
Olivia Kesler <oakesler@protonmail.ch>
2019-12-29 07:40:56 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,13 @@ require 'sinatra/base'
 class App < Sinatra::Base
 
     set :views, Proc.new { File.join(root, "../views/") }
-
+    
+    get "/" do 
+      erb :new
+    end
+    
+    post "/show" do 
+      erb :show
+    end
 
 end
