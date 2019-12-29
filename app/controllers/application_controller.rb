5c293bb4fe914d6acb5d67be85675aff809ea294
Olivia Kesler <oakesler@protonmail.ch>
2019-12-29 07:50:45 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,9 @@
 require 'sinatra/base'
 
+require './config/environment.rb'
+require './app/models/team.rb'
+require './app/models/hero.rb'
+
 class App < Sinatra::Base
 
     set :views, Proc.new { File.join(root, "../views/") }
@@ -9,7 +13,20 @@ class App < Sinatra::Base
     end
     
     post "/show" do 
+      @team = 
       erb :show
     end
 
 end
+
+ #post '/pirates' do
+      #@pirate = Pirate.new(params[:pirate])
+      
+      #params[:pirate][:ships].each do |details|
+        #Ship.new(details)
+      #end
+      
+      #@ships = Ship.all
+
+      #erb :"pirates/show"
+  #end
